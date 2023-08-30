cd97c3223352e346feabd3582477675d15126718
sitra fethi <sitra.f.khairo515@gmail.com>
2023-08-30 20:37:30 +0300

    new commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Heading from "./heading";
+import Card from "./card";
+import Navigation from './nav';
+function App() {
+  return (
+   <div className='section'>
+    <Navigation/>
+    <Heading/>
+    <Card/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/heading.js
@@ -0,0 +1,8 @@
+function Heading(){
+
+    return(
+        <h1>Welcome  To our Website</h1>
+    );
+}
+
+export default  Heading;
